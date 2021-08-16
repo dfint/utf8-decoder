9cbfdad7ca958399371cad405bca6e4637ca5f06
insolor <insolor@gmail.com>
2021-08-16 20:24:29 +0300

    Move repeated part into a function

--- decode_utf8.py
+++ decode_utf8.py
@@ -1,3 +1,8 @@
+def shift_1(s, w):
+    c = s.pop(0)
+    return w << 6 | (c & 0x3F)
+
+
 def decode_utf8(s: bytes):
     s = list(s)
     while s:
@@ -6,59 +11,29 @@ def decode_utf8(s: bytes):
             w = c
         elif c < 0xE0:
             w = c & 0x1F
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
+            w = shift_1(s, w)
         elif c < 0xF0:
             w = c & 0x0F
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
         elif c < 0xF8:
             w = c & 0x07
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
         elif c < 0xFC:
             w = c & 0x03
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
         else:
             w = c & 1
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
-            
-            c = s.pop(0)
-            w = w << 6 | (c & 0x3F)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
+            w = shift_1(s, w)
         
         if w < 0x10000:
             yield w
