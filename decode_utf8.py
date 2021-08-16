96c338844c4c7888aecd90853d579a9e2b557dff
insolor <insolor@gmail.com>
2021-08-16 21:05:22 +0300

    Add functions for multiple shifts

--- decode_utf8.py
+++ decode_utf8.py
@@ -3,6 +3,27 @@ def shift_1(s, w):
     return w << 6 | (c & 0x3F)
 
 
+def shift_2(s, w):
+    w = shift_1(s, w)
+    return shift_1(s, w)
+
+
+def shift_3(s, w):
+    w = shift_1(s, w)
+    return shift_2(s, w)
+
+
+def shift_4(s, w):
+    w = shift_1(s, w)
+    return shift_3(s, w)
+
+
+def shift_5(s, w):
+    w = shift_1(s, w)
+    return shift_4(s, w)
+
+
+
 def decode_utf8(s: bytes):
     s = list(s)
     while s:
@@ -14,26 +35,16 @@ def decode_utf8(s: bytes):
             w = shift_1(s, w)
         elif c < 0b1111_0000:
             w = c & 0b0000_1111
-            w = shift_1(s, w)
-            w = shift_1(s, w)
+            w = shift_2(s, w)
         elif c < 0b11111_000:
             w = c & 0b00000_111
-            w = shift_1(s, w)
-            w = shift_1(s, w)
-            w = shift_1(s, w)
+            w = shift_3(s, w)
         elif c < 0b111111_00: 
             w = c & 0b000000_11
-            w = shift_1(s, w)
-            w = shift_1(s, w)
-            w = shift_1(s, w)
-            w = shift_1(s, w)
+            w = shift_4(s, w)
         else:  # < 0b1111111_0
             w = c & 0b0000000_1
-            w = shift_1(s, w)
-            w = shift_1(s, w)
-            w = shift_1(s, w)
-            w = shift_1(s, w)
-            w = shift_1(s, w)
+            w = shift_5(s, w)
         
         if w < 0x10000:
             yield w
