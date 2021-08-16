3fb9b6051ac36820aa25453a9b6a322f5abe5a74
insolor <insolor@gmail.com>
2021-08-16 19:28:47 +0300

    Simplify code using list.pop

--- decode_utf8.py
+++ decode_utf8.py
@@ -1,80 +1,64 @@
 def decode_utf8(s: bytes):
-    i = 0
-    while i < len(s):
-        c = s[i]
+    s = list(s)
+    while s:
+        c = s.pop(0)
         w = c
-        i += 1
         if c > 0xC0:
             if c < 0xE0:
                 w = c & 0x1F
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
             elif c < 0xF0:
                 w = c & 0x0F
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
             elif c < 0xF8:
                 w = c & 0x07
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
             elif c < 0xFC:
                 w = c & 0x03
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
             else:
                 w = c & 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
                 
-                c = s[i]
+                c = s.pop(0)
                 w = w << 6 | (c & 0x3F)
-                i += 1
         
         if w < 0x10000:
             yield w
