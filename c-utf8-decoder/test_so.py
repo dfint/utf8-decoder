a5dd8d6cd7aedadee5bd028b4c91184730cb911f
insolor <insolor@gmail.com>
2021-08-16 23:51:18 +0300

    Add a test for so (not working yet)

---
+++ c-utf8-decoder/test_so.py
@@ -0,0 +1,23 @@
+import os
+
+from ctypes import *
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def libutf8_decoder():
+    return cdll.LoadLibrary("libutf8_decoder.so")
+
+
+@pytest.mark.parametrize("text", [
+    "text"
+])
+def test_utf8_decoder(libutf8_decoder, text):
+    lib = libutf8_decoder
+    array_size = len(text.encode('utf-16-le')) // 2
+    buffer = (c_ulong * array_size)()
+    result = lib.decode_utf8(buffer, text)
+    print(buffer - result)
+    assert False
