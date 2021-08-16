eee08533be06cae8a4a505fed3cb73a7d9572427
insolor <insolor@gmail.com>
2021-08-16 17:15:36 +0300

    Initial commit

---
+++ decode_utf8.py
@@ -0,0 +1,58 @@
+def decode_utf8(s: bytes):
+    i = 0
+    while i < len(s):
+        c = s[i]
+        w = c
+        i += 1
+        if c > 0xC0:
+            if c < 0xE0:
+                w = c & 0x1F
+                c = s[i]
+                w = w << 6 | (c & 0x3F)
+                i += 1
+            elif c < 0xF0:
+                w = c & 0x0F
+                c = s[i]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+1]
+                w = w << 6 | (c & 0x3F)
+                i += 2
+            elif c < 0xF8:
+                w = c & 0x07
+                c = s[i]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+1]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+2]
+                w = w << 6 | (c & 0x3F)
+                i += 3
+            elif c < 0xFC:
+                w = c & 0x03
+                c = s[i]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+1]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+2]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+3]
+                w = w << 6 | (c & 0x3F)
+                i += 4
+            else:
+                w = c & 1
+                c = s[i]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+1]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+2]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+3]
+                w = w << 6 | (c & 0x3F)
+                c = s[i+4]
+                w = w << 6 | (c & 0x3F)
+                i += 5
+        
+        if w < 0x10000:
+            yield w
+        else:
+            yield 0xD7C0 | (w >> 10)
+            yield 0xDC00 | (w & 0x3FF)
---
+++ test_decode_utf8.py
@@ -0,0 +1,14 @@
+import pytest
+
+from decode_utf8 import decode_utf8
+
+@pytest.mark.parametrize("text",
+[
+    "Привет!",
+    "你好"
+])
+def test(text):
+    encoded_utf8 = text.encode('utf-8')
+    utf16_byte_pairs = decode_utf8(encoded_utf8)
+    utf16_le_bytes = b''.join(map(lambda x: x.to_bytes(2, byteorder='little'), utf16_byte_pairs))
+    assert utf16_le_bytes == text.encode('utf-16-le')
