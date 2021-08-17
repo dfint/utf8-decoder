84aca3a8255ba0480ce17a5f44ab23b4e410eaf2
insolor <insolor@gmail.com>
2021-08-17 16:54:17 +0300

    Improve test for so

--- c-utf8-decoder/test_so.py
+++ c-utf8-decoder/test_so.py
@@ -21,13 +21,14 @@ def libutf8_decoder():
 def test_utf8_decoder(libutf8_decoder, text):
     lib = libutf8_decoder
     lib.decode_utf8.restype = c_void_p
+    
     array_size = len(text.encode('utf-16-le')) // 2
     buffer = (c_uint32 * array_size)()
 
-    result = lib.decode_utf8(buffer, c_char_p(text.encode('utf-8')))
+    result = lib.decode_utf8(buffer, text.encode('utf-8'))
 
-    result_size = result - addressof(buffer)
-    assert result_size // 4 == array_size
+    result_size = (result - addressof(buffer)) // sizeof(c_uint32)
+    assert result_size == array_size
 
     print(list(buffer))
     utf16_le_bytes = b''.join(map(lambda x: x.to_bytes(2, byteorder='little'), buffer))
